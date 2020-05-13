b235ddc48d43b81c66a79c44c347c43df5f11467
Charles Delannoy <charles.delannoy@hei.fr>
2020-05-13 15:35:24 +0200

    restoManager ok

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
-  validates :category, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :category, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"],
+                                     message: 'must be "chinese", "italian", "japanese", "french" or "belgian"' }
 end
